1d11ab57f9396741949ca097af50e4243e35987b
sspillane1 <45501674+sspillane1@users.noreply.github.com>
2019-10-12 15:24:42 -0400

    fedcwqc

--- flask-server.py
+++ flask-server.py
@@ -1,16 +1,67 @@
 from flask import Flask, send_file, request
+from bs4 import BeautifulSoup
+import json
+import requests
+
+from pyteaser import SummarizeUrl
+
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
+def scraper(query):
+    url_list = []
+    # Grab url of news site to be searched and add search query
+    # Can add a switch statement if we want more sites
+    url = "https://news.google.com/search?q={}&hl=en-US&gl=US&ceid=US%3Aen".format(query)
+    response = requests.get(url)
+    data = response.text
+    browser = BeautifulSoup(data, 'lxml')
+    # print(browser.get_text())
+    raw = []
+    # Generate array of only urls from page
+    for link in browser.find_all('a'):
+        if "./" in str(link.get('href')):
+            raw.append(str(link.get('href')))
+    # Check for dupes
+    i = 0
+    while i < len(raw):
+        try:
+            if raw[i] == raw[i + 1]:
+                del raw[i]
+
+            # Comment plez
+            url_list.append('https://news.google.com/{}'.format(raw[i]))
+        except IndexError:
+            # Comment plez
+            url_list.append('https://news.google.com/{}'.format(raw[i]))
+        i += 1
+
+    for q in range(12):
+        url_list.pop(0)
+
+    return tuple(url_list)
+
+def createjson(st):
+    x = json.dumps(st)
+    with open("data.json", "w") as write_file:
+        json.dump(x, write_file)
+    print(json.dumps(x))
+
+
+
+@app.route('/getinfo', methods=['GET'])
+def getInfo(loc=None, top=None):
+    output = scraper("homeless%20boston")
+
+
+    for url in output:
+        summaries = SummarizeUrl(url)
+
+    ret=createjson(summaries)
+
+    #loc, topic
+    #art
+
     # the code below is executed if the request method
     # was GET or the credentials were invalid
-    return render_template('login.html', error=error)
+    return ret
--- scraper.py
+++ scraper.py
@@ -14,39 +14,6 @@ from pprint import pprint
 # def __init__(self, q):
 # #     self.query = q
 
-def scraper(query):
-    url_list = []
-    # Grab url of news site to be searched and add search query
-    # Can add a switch statement if we want more sites
-    url = "https://news.google.com/search?q={}&hl=en-US&gl=US&ceid=US%3Aen".format(query)
-    response = requests.get(url)
-    data = response.text
-    browser = BeautifulSoup(data, 'lxml')
-    # print(browser.get_text())
-    raw = []
-    # Generate array of only urls from page
-    for link in browser.find_all('a'):
-        if "./" in str(link.get('href')):
-            raw.append(str(link.get('href')))
-    # Check for dupes
-    i = 0
-    while i < len(raw):
-        try:
-            if raw[i] == raw[i + 1]:
-                del raw[i]
-
-            # Comment plez
-            url_list.append('https://news.google.com/{}'.format(raw[i]))
-        except IndexError:
-            # Comment plez
-            url_list.append('https://news.google.com/{}'.format(raw[i]))
-        i += 1
-
-    for q in range(12):
-        url_list.pop(0)
-
-    return tuple(url_list)
-
 
 # Comment this stuff out
 if __name__ == "__main__":
@@ -58,6 +25,8 @@ if __name__ == "__main__":
     urls = (u'https://ktvl.com/news/local/police-help-homeless-people-find-permanent-housing',
             u'http://www.bbc.co.uk/news/world-europe-30035666',
             u'http://www.bbc.co.uk/news/magazine-29631332')
+
+
 
 
     for url in lis:
